bb9623eb8e0553229e07cd61f90fc60b6faefd41
Ermek999 <139259757+Ermek999@users.noreply.github.com>
2023-07-22 17:29:26 +0300

    Example001_3.1

---
+++ Example001_3.1/Program.cs
@@ -0,0 +1,26 @@
+﻿// нАПИШИТЕ ПРОГРАММУ, КОТОРАЯ НА ВХОД ПРИНИМАЕТ ДВА ЧИСЛА И ВЫДАЕТ,какое число большее, ам какоее меньшее
+
+Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32((Console.ReadLine()));
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32((Console.ReadLine()));
+
+
+
+if (num1>num2)
+{
+
+    Console.WriteLine($"число {num1} больше чем число {num2}");
+}
+else if (num1<num2)
+{
+    Console.WriteLine($"число {num1} меньше чем число {num2}");
+}
+
+else
+{
+
+    Console.WriteLine($"число {num1} и {num2} равны");
+}
+
+
