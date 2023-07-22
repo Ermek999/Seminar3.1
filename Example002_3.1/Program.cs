d6886015366c69af508871808dfc3e608745d21a
Ermek999 <139259757+Ermek999@users.noreply.github.com>
2023-07-22 19:17:17 +0300

    Example002_3.1

--- Example002_3.1/Program.cs
+++ Example002_3.1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число ");
+int num1 = int.Parse((Console.ReadLine()));
+Console.Write("Введите второе число ");
+int num2 = int.Parse((Console.ReadLine()));
+Console.Write("Введите третье число ");
+int num3 = int.Parse((Console.ReadLine()));
+
+int max = num1;
+if (num1>max) max = num1;
+if (num2>max) max= num2;
+if (num3>max) max= num3;
+
+Console.WriteLine (max);
