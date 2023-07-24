b4a501b858258e2f975182369c97a5659947f572
Ermek999 <139259757+Ermek999@users.noreply.github.com>
2023-07-24 22:18:57 +0300

    Переименовал

---
+++ Example3.1_004/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Напишите число ");
+int number= int.Parse(Console.ReadLine());
+
+
+int number2 = 1;
+
+while (number2 <= number) 
+{
+    
+    {    Console.Write($"{number2} ");
+        number2++;
+    }
+}
+
+
+
+
+    
