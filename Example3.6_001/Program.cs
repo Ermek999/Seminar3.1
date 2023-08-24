bc007d12cf01df0b6c4095c42d98b0543b045a9a
Ermek999 <139259757+Ermek999@users.noreply.github.com>
2023-08-24 23:26:02 +0300

    Example3.6_001

---
+++ Example3.6_001/Program.cs
@@ -0,0 +1,40 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите количество элементов массива: ");
+int m = int.Parse(Console.ReadLine());
+int[] Array = new int[m];
+mas(m);
+Console.Write($"Чисел больше нуля: {quantity(Array)}");
+void mas(int m) {
+for (int i = 0; i < m; i++){
+Console.WriteLine($"Введите {i+1} элемент массива ");
+Array[i] = Convert.ToInt32(Console.ReadLine());
+}
+}
+
+int quantity(int[] Array){
+int i=0;
+int sum = 0;
+while (i < Array.Length){
+if(Array[i]>0)
+sum ++;
+i++;
+}
+return sum;
+}
+
+
+// Console.WriteLine("Введите переменную b1: ");
+// double b1 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите переменную k1: ");
+// double k1 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите переменную b2: ");
+// double b2 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите переменную k2: ");
+// double k2 = int.Parse(Console.ReadLine());
+
+
+
+// double x = -(b1 - b2) / (k1 - k2);
+// double y = k1 * x + b1;
+
+// Console.Write($" Точка пересечения двух прямых: [{x},{y}]");
