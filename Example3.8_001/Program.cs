a022f84f9b38146b0b97b988d121560adb8516e2
Ermek999 <139259757+Ermek999@users.noreply.github.com>
2023-09-03 11:16:27 +0300

    Example3.9_001

---
+++ Example3.8_001/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Console.WriteLine($"Введите количество строк");
+int m = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Введите количество столбцов");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] matrix = GetArray(m, n, 0, 10);
+PrintArray(matrix);
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+    {
+        for (int z = 0; z < matrix.GetLength(1) - 1; z++)
+        {
+            if (matrix[i, z] < matrix[i, z + 1]) //для изменения сортировки поменять знак < на противоположный
+            {
+                int temp = 0;
+                temp = matrix[i, z];
+                matrix[i, z] = matrix[i, z + 1];
+                matrix[i, z + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными значениями");
+PrintArray(matrix);
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(minValue, maxValue +1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Example3.9-001/Program.cs
@@ -0,0 +1,51 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// Console.WriteLine("Введите число ");
+// int numbers = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(Ansver(numbers, 1));
+
+// string Ansver(int A, int B)
+// {  
+//     if(A == 1) return A.ToString();
+//     return (A + " , " + Ansver(A - 1, B)); 
+// }
+
+// Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+// Console.WriteLine("Введите число ");
+// int m = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите число ");
+// int n = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine(STemp(m,n));
+// int STemp( int m, int n)
+// {   
+//         if(m == n) return n;
+//     return(m + STemp(m + 1,n));
+// }
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(Akkerman(m,n));
+
+// // функция Аккермана
+// int Akkerman( int m, int n)
+// {
+//     if (m == 0) return n + 1;
+//     else if (n == 0 && m > 0)
+//     {
+//         return Akkerman(m - 1, 1);
+//     }
+//     else
+//     {
+//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
+//     }
+// }
